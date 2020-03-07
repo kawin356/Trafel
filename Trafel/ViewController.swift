4e2bbedc8b452b0b4d79ac4bed26aa9f6a4560d8
Kittkawin <kawin356@gmail.com>
2020-03-07 16:54:00 +0700

    Setup Storyboard and some Seague

---
+++ Trafel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trafel
+//
+//  Created by Kittikawin Sontinarakul on 7/3/2563 BE.
+//  Copyright © 2563 Kittikawin Sontinarakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
