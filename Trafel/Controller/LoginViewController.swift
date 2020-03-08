1086e82f136cfcecb2ab899b9730f779639afca1
Kittikawin <kawin356@gmail.com>
2020-03-08 16:31:09 +0700

    Edit Login/Signup Storyboard

---
+++ Trafel/Controller/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Trafel
+//
+//  Created by Kittikawin Sontinarakul on 8/3/2563 BE.
+//  Copyright © 2563 Kittikawin Sontinarakul. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
