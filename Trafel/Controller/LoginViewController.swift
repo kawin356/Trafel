62521d26dc8faa365255bc2518b1c34b3b953d61
Kittikawin <kawin356@gmail.com>
2020-03-08 19:35:40 +0700

    Completed segment Button (login/Signup)

--- Trafel/Controller/LoginViewController.swift
+++ Trafel/Controller/LoginViewController.swift
@@ -9,9 +9,62 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordComfirmationTextfield: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var forgetPasswordButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViewFor(pageType: .login)
     }
     
+    private enum PageType {
+        case login
+        case signup
+    }
+    
+    private var currentPageType: PageType = .login {
+        didSet {
+            setupViewFor(pageType: currentPageType)
+        }
+    }
+    
+    private func setupViewFor(pageType: PageType){
+        passwordComfirmationTextfield.isHidden = pageType == .login
+        signupButton.isHidden = pageType == .login
+        loginButton.isHidden = pageType == .signup
+        forgetPasswordButton.isHidden = pageType == .signup
+        
+        switch pageType {
+        case .login:
+            passwordComfirmationTextfield.isHidden = true
+            signupButton.isHidden = true
+            errorLabel.isHidden = true
+        case .signup:
+            signupButton.isHidden = false
+            passwordComfirmationTextfield.isHidden = false
+            loginButton.isHidden = true
+            forgetPasswordButton.isHidden = true
+            errorLabel.isHidden = true
+        }
+    }
+    
+    @IBAction func segmentControlPressed(_ sender: UISegmentedControl) {
+        currentPageType = sender.selectedSegmentIndex == 0 ? .login : .signup
+    }
+    
+    
+    @IBAction func forgetPasswordButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+    }
 }
